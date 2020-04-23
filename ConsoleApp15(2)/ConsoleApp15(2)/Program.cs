ad14b128793942bc01174aa0fdebc39f8022c752
YaroslavTos <63033614+YaroslavTos@users.noreply.github.com>
2020-04-23 16:03:19 +0600

    Add files via upload

---
+++ ConsoleApp15(2)/ConsoleApp15(2)/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите номер буквы  : ");
+            int x = int.Parse(Console.ReadLine());
+
+            
+
+            switch (x)
+            {
+                case 1:
+                    Console.WriteLine("А");
+                    break;
+                case 2:
+                    Console.WriteLine("Б");
+                    break;
+                case 3:
+                    Console.WriteLine("В");
+                    break;
+
+                default:
+                    Console.WriteLine("Нету такой буквы ");
+                    break;
+            }
+         
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp16(2)/ConsoleApp16(2)/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число от 1 до 10 : ");
+            int x = int.Parse(Console.ReadLine());
+
+            switch (x)
+            {
+                case 1:
+                    Console.WriteLine("1");
+                    Console.WriteLine(x * x * x);
+                    break;
+                case 2:
+                    Console.WriteLine("2");
+                    Console.WriteLine(x * x * x);
+                    break;
+                case 3:
+                    Console.WriteLine("3");
+                    Console.WriteLine(x * x * x);
+
+                    break;
+
+                case 4:
+                    Console.WriteLine("4");
+                    
+                    break;
+                case 5:
+                    Console.WriteLine("5");
+                    Console.WriteLine(x * x * x);
+                    break;
+                case 6:
+                    Console.WriteLine("6");
+                    
+                    break;
+
+                case 7:
+                    Console.WriteLine("7");
+                    Console.WriteLine(x * x * x);
+                    break;
+                case 8:
+                    Console.WriteLine("8");
+                    
+                    break;
+                case 9:
+                    Console.WriteLine("9");
+                    Console.WriteLine(x * x * x);
+                    break;
+
+                case 10:
+                    Console.WriteLine("10");
+                    
+                    break;
+                
+
+                default:
+                    Console.WriteLine("ERROR");
+                    break;
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
